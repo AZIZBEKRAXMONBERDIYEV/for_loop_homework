a7afff94a39477b150ed24ab498a3c6ce40abf6b
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-01-27 11:55:31 +0500

    code07

--- For07.py
+++ For07.py
@@ -6,4 +6,11 @@ def main(N):
     Returns:
         int: return  answer
     """
-    return
+    s=0
+    for i in range(N):
+        if i%2==1:
+            s+=i
+        
+    return s
+print(main(12))
+    
